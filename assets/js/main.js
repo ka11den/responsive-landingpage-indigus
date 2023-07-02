e933cf097e14521a3fb9bcde78990f8b4281d373
ka11den <ka1den@mail.ru>
2023-07-02 21:34:08 +0400

    fix: responsive small device, animation, slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,29 +8,10 @@ if (navToggle) {
     navMenu.classList.add('snow-menu');
   })
 }
-
+  
 if (navClose) {
   navClose.addEventListener('click', () => {
     navMenu.classList.remove('snow-menu');
-  })
-}
-
-// scroll
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-  const scrollY = window.pageYOffset
-
-  sections.forEach(current => {
-    const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute('id')
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-    } else {
-      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-    }
   })
 }
 
@@ -108,123 +89,87 @@ const teams = [
   {
     id: 1,
     fio: "Степанова Татьяна Юрьевна",
-    desc: "",
-    img: "./assets/img/teams__1.png",
+    desc: "Педагог английского языка На протяжении многих лет педагог осуществляет успешную подготовку не только обучающихся с 1 по 11 классы в рамках школьной программы.",
+    img: "./assets/img/teams__1.webp",
     cat: "Английский Язык"
   },
   {
     id: 2,
     fio: "Рачинская Ольга Игоревна",
-    desc: "",
-    img: "./assets/img/teams__22.png",
+    desc: "Педагог дополнительного образования В 2013 году окончила АГУ по специальности «Иностранный язык»",
+    img: "./assets/img/teams__22.webp",
     cat: "Английский Язык"
   },
   {
     id: 3,
     fio: "Бойдиэль София Мохамеденовна",
-    desc: "",
-    img: "./assets/img/teams__7.jpg",
+    desc: "Профиль подготовки - педагог английского и французского языков Победитель международного конкурса Релод и региональных олимпиад по французскому языку",
+    img: "./assets/img/teams__7.webp",
     cat: "Английский Язык"
   },
   {
     id: 4,
     fio: "Савинова Елена Юрьевна",
-    desc: "",
-    img: "./assets/img/teams__4.jpg",
+    desc: "Учитель начальных классов Образование – высшее: Астраханский государственный педагогический институт им. С.М.Кирова , 1994 г.Астраханский государственный университет.",
+    img: "./assets/img/teams__4.webp",
     cat: "Начальные классы"
   },
   {
     id: 5,
     fio: "Данилова Анна Владимировна",
-    desc: "",
-    img: "./assets/img/teams__2.jpg",
+    desc: "Учитель иностранного языка Диплом о высшем образовании с отличием (бакалавриат). 2015-2019 - Калмыцкий государственный университет им.Б.Б.",
+    img: "./assets/img/teams__2.webp",
     cat: "Английский Язык"
   },
   {
     id: 6,
     fio: "Улюмджиева Гиляна Саналовна",
-    desc: "",
-    img: "./assets/img/teams__6.jpg",
+    desc: "Учитель иностранного языка Диплом о высшем образовании с отличием (бакалавриат). 2015-2019 - Калмыцкий государственный университет им.Б.Б.",
+    img: "./assets/img/teams__6.webp",
     cat: "Английский Язык"
   },
   {
     id: 7,
     fio: "Секерина Марина Игоревна",
-    desc: "",
-    img: "./assets/img/teams__8.jpg",
+    desc: "Педагог по английскому языку Образование - высшее, Астраханский государственный университет, 2009 г. Магистратура по направлению — Педагогика, 2011 г.Педагогический стаж работы - 13 лет.",
+    img: "./assets/img/teams__8.webp",
     cat: "Английский Язык"
   },
   {
     id: 8,
     fio: "Кораблева Елена Павловна",
-    desc: "",
-    img: "./assets/img/teams__5.jpg",
+    desc: "Педагог русского языка Образование – высшее: Астраханский педагогический институт, 2000г. Магистратура по направлению – Педагогическое образование, 2015г. Стаж работы по специальности – 15 лет.",
+    img: "./assets/img/teams__5.webp",
     cat: "Английский Язык"
   },
   {
     id: 9 ,
     fio: "Карпова Полина Евгеньевна",
     desc: "",
-    img: "./assets/img/teams__3.jpg",
+    img: "./assets/img/teams__3.webp",
     cat: "Английский Язык"
   }
 ]
 
-const teamsContent = document.querySelector(".teams__slider-content");
-const teamsCat = document.querySelector(".teams__list");
+const teamsContent = document.querySelector(".teams__container");
 
 const displayTeams = (filterTeams) => {
-  teamsContent.innerHTML = filterTeams.map
-  (
-    (teams) =>
-      `
-        <article class="teams__slider-card teams__slider-card-container container grid">
-          <div class="teams__slider-card-data">
-            <h1 class="teams__slider-card-title">
-              ${teams.fio}
-            </h1>
-            <p class="teams__slider-card-desc">
-              Педагог английского языка. На протяжении многих лет педагог осуществляет успешную обучающихся с 1 по 11 классы в рамках школьной программы. 
-            </p>
-          </div>
-          <img class="teams__slider-card-img" src=${teams.img} />
-        </article>
-      `
-    )
-    .join("");
+  teamsContent.innerHTML = filterTeams.map((teams) =>
+    `
+    <article class="teams__slider-card teams__slider-card-container grid">
+      <div class="teams__slider-card-data">
+        <h1 class="teams__slider-card-title">
+          ${teams.fio}
+        </h1>
+        <p class="teams__slider-card-desc">
+          ${teams.desc}
+        </p>
+      </div>
+      <img class="teams__slider-card-img" src=${teams.img} />
+    </article>
+    `
+  )
+  .join("");
 };
 
-const setCategories = () => {
-  const allCats = teams.map((item) => item.cat);
-  const categories = [
-    "Все",
-
-    ...allCats.filter((item, i) => {
-      return allCats.indexOf(item) === i;
-    }),
-  ];
-
-  teamsCat.innerHTML = categories
-    .map
-    (
-      (cat) =>
-        `
-          <li class="teams__item">
-            <a class="teams__link">${cat}</a>
-          </li>
-        `
-    )
-    .join("");
-
-    teamsCat.addEventListener("click", (e) => {
-    const selectedCat = e.target.textContent;
-
-    selectedCat === "Все"
-      ? displayTeams(teams)
-      : displayTeams(teams.filter((item) => item.cat === selectedCat));
-  });
-};
-
-setCategories()
 displayTeams(teams);
-window.addEventListener('scroll', scrollActive)
